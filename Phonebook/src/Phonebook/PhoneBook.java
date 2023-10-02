d8207af9dc1da73999c70b0ed5ff4ef79c3fe30d
sarah <sara.dj.2024@gmail.com>
2023-10-02 14:13:24 +0300

    added command lines

--- Phonebook/src/Phonebook/PhoneBook.java
+++ Phonebook/src/Phonebook/PhoneBook.java
@@ -1,10 +1,151 @@
 package Phonebook;
-
+import java.util.Scanner;
 public class PhoneBook {
     LinkedList<Contact> ContactLinkedList = new LinkedList<>();
+    public static void main (String[]args){
+        String choice="0";
+        while(choice!="8"){ //loop is not working :-D wouldnt stop at 8 :-D
+        System.out.println("Welcome to the Linked Tree Phonebook!\r\n" + 
+                "Please choose an option:\r\n" + 
+                "1. Add a contact\r\n" + 
+                "2. Search for a contact\r\n" + 
+                "3. Delete a contact\r\n" + 
+                "4. Schedule an event\r\n" + 
+                "5. Print event details\r\n" + 
+                "6. Print contacts by first name\r\n" + 
+                "7. Print all events alphabetically\r\n" + 
+                "8. Exit");
+
+                Scanner scan = new Scanner(System.in);
+
+                System.out.print("Enter your choice:");
+                choice = scan.nextLine();
+
+                switch (choice){
+                    case "1":
+                    //create new object contact and link each attribute scanned ie obj.name =name;
+                    Contact c1 = new Contact();
+                    System.out.print("Enter the contact's name:");
+                    String name = scan.nextLine();
+                    c1.setName(name);
+                    System.out.print("Enter the contact's phone number:");
+                    String number = scan.nextLine();
+                    c1.setPhoneNumber(number);
+                    System.out.print("Enter the contact's email address:");
+                    String email = scan.nextLine();
+                    c1.setEmail(email);
+                    System.out.print("Enter the contact's address:");
+                    String address = scan.nextLine();
+                    c1.setAddress(address);
+                    System.out.print("Enter the contact's birthday:");
+                    String birthday = scan.nextLine();
+                    c1.setBirthday(birthday);
+                    System.out.print("Enter any notes for the contact:");
+                    String notes = scan.nextLine();
+                    c1.setNotes(notes);
+                    System.out.println("\nContact added successfully!");
+
+                    break;
+
+                    case "2":
+                    System.out.println("Enter search criteria:\n1.Name\n2.Phone Number\n3.Email Address\n4.Address\n5.Birthday");
+                    String input = scan.nextLine();
+
+                    switch (input){ //nested switch
+                        case "1":
+                        System.out.print("Enter the contact's name:");
+                        String contactName = scan.nextLine();
+                        /*search by name method will be used here
+                         * if(contactLinkedList.searchbyname) ??????????? idk
+                         */
+                        break;
+
+                        case "2":
+                        System.out.print("Enter the contact's phone number:");
+                        String contactPhoneNumber = scan.nextLine();
+                        //search
+                        break;
+
+                        case "3":
+                        System.out.print("Enter the contact's email address:");
+                        String contactEmailAddress = scan.nextLine();
+                        //search
+                        break;
+
+                        case "4":
+                        System.out.print("Enter the contact's address:");
+                        String contactAddress = scan.nextLine();
+                        //search
+                        break;
+
+                        case "5":
+                        System.out.print("Enter the contact's birthday:");
+                        String contactBirthday = scan.nextLine();
+                        //search
+                        break;
+                    }
+
+                    break;
+
+                    case "3": //delete contact
+                    break;
+
+                    case "4": //schedule an event
+                    System.out.print("Enter event title:");
+                    String eventTitle = scan.nextLine();
+                    System.out.print("Enter contact name:");
+                    String contactName = scan.nextLine();
+                    System.out.print("Enter event date and time (MM/DD/YYYY HH:MM):");
+                    String eventTimeAndDate = scan.nextLine();
+                    System.out.print("Enter event location:");
+                    String eventLocation = scan.nextLine();
+
+                    //maybe i should search for contact by name here first then add it in the method,
+
+                    //Event e = new Event(eventTitle, eventTimeAndDate, eventLocation, c1); //how should i enter contact name?
+                        
+                    break;
+
+                    case "5": //print event detail
+                    System.out.print("Enter search criteria:\n1.contact name\n2.event title:");
+                    int eventInput = scan.nextInt();
+
+                    switch(eventInput){
+                        case 1:
+                        System.out.print("Enter contact name:");
+                        String eventContactName = scan.nextLine();
+                        // eventList.SearchByContactName(eventContactName); //what am i supposed to do??
+                        break;
+
+                        case 2:
+                        System.out.print("Enter event title:");
+                        String eventTitlePrint = scan.nextLine();
+                        //search
+                        break;
+                    }
+
+                    break;
+
+                    case "6": //print contacts by first name
+
+                    System.out.print("Enter the first name:");
+                    String firstName = scan.nextLine();
+                    //what method to use?
+                    break;
+
+                    case "7": //print all events alphabetically
+                    break;
+
+                    case "8":
+                    return;
+
+
+                }
+            }
 
 
 
+    }
   
     public boolean AddContact(Contact contact){
         if(ContactLinkedList.empty())

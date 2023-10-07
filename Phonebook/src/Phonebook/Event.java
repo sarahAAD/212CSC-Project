3b241de157b7764a1f4d4fd1d1653110320e4fac
Raghad <145941111+Raghadr9@users.noreply.github.com>
2023-10-08 01:32:09 +0300

    Update Event.java

--- Phonebook/src/Phonebook/Event.java
+++ Phonebook/src/Phonebook/Event.java
@@ -5,7 +5,7 @@ public class Event extends PhoneBook {
 	private String DateAndTime;
 	private String location;
 	private Contact contact;
-	private LinkedListQueue eventList = new LinkedListQueue();
+	public LinkedList<Event> eventList = new LinkedList<>();
 	LinkedList<Contact> ContactList = new LinkedList<>();
 
 	public Event() {
@@ -19,56 +19,7 @@ public class Event extends PhoneBook {
 
 	}
 
-	public void addEvent(Event event) {
-		if (!isExist(event)) {
-			eventList.enqueue(event);
-		System.out.println("event added successfully!");}
-		else
-			System.out.println("This contact doesn't exist, can't add the event");
 
-	}
-
-	public Event SearchByTitle(String Title) {
-		Event temp = null;
-		if (eventList.length() != 0) {
-			int size = eventList.length();
-			for (int i = 1; i <= size; i++) {
-				temp = eventList.serve();
-				eventList.enqueue(temp);
-				if (temp.eventTitle.equalsIgnoreCase(Title)) {
-				}
-			}
-		}
-		return temp;
-
-	}
-
-	public Event SearchByContactName(String Name) {
-		if (eventList.length() != 0) {
-			int size = eventList.length();
-			for (int i = 1; i <= size; i++) {
-				Event temp = eventList.serve();
-				if (temp.contact.getName().equalsIgnoreCase(Name)) {
-					eventList.enqueue(temp);
-					return temp;
-				}
-			}
-		}
-		System.out.println("This event doesn't exist");
-		return null;
-	}
-
-	public void alphabrticOrder(String name) {
-		LinkedListQueue events = new LinkedListQueue();
-		Event tmp = eventList.serve();
-		events.enqueue(tmp);
-		for (int i = 0; i < eventList.length(); i++) {
-			if (tmp.eventTitle.charAt(0) >= eventList.serve().eventTitle.charAt(0)) {
-
-			}
-		}
-
-	}
 
 	public boolean isExist(Event event) {
 		if (ContactLinkedList.empty())
@@ -127,17 +78,17 @@ public class Event extends PhoneBook {
 		this.contact = contact;
 	}
 
-	public LinkedListQueue getEventList() {
+	public LinkedList getEventList() {
 		return eventList;
 	}
 
-	public void setEventList(LinkedListQueue eventList) {
+	public void setEventList(LinkedList eventList) {
 		this.eventList = eventList;
 	}
 
 	public String toString() {
-		return "eventTitle:" + eventTitle + "\nDateAndTime:" + DateAndTime + "\nlocation:" + location + "\ncontact:"
-				+ contact + "\neventList:" + eventList + "\nContactList:" + ContactList;
+		return "eventTitle:" + eventTitle + "\nDateAndTime:" + DateAndTime + "\nlocation:" + location + "\ncontact name:"
+				+ contact.getName() ;
 	}
 
 }

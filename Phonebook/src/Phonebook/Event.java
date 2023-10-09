9fcf03e848bcb09c5853efe971dd54be44c9cc15
sarahAAD <145826237+sarahAAD@users.noreply.github.com>
2023-10-09 23:17:14 +0300

    Update Event.java// added method Exists

--- Phonebook/src/Phonebook/Event.java
+++ Phonebook/src/Phonebook/Event.java
@@ -5,34 +5,29 @@ public class Event extends PhoneBook {
 	private String DateAndTime;
 	private String location;
 	private Contact contact;
-	public LinkedList<Event> eventList = new LinkedList<>();
-	LinkedList<Contact> ContactList = new LinkedList<>();
-
-	public Event() {
-	}
+	//public LinkedList<Event> eventList;
+	
+	public Event(){
+    };
 
 	public Event(String eventTitle, String DateAndTime, String location, Contact c) {
 		this.eventTitle = eventTitle;
 		this.DateAndTime = DateAndTime;
 		this.location = location;
 		contact = new Contact(c);
-
 	}
 
-/* public int compareTo(Event val) {
-		return eventTitle.compareTo(val.eventTitle);
-	}
-	*/
 
-	public boolean isExist(Event event) {
+
+    public boolean isExist(Event event) {                                                            //practically made to answer the question of wether a contact has a certain event  or not
 		if (ContactLinkedList.empty())
 			return false;
 		else {
 			ContactLinkedList.FindFirst();
 			while (!ContactLinkedList.last()) {
-				if (ContactLinkedList.retrieve().getEvent() != null) {
+				if (ContactLinkedList.retrieve().getEvent() != null) {                                  //if the contact actually has any events
 					if (ContactLinkedList.retrieve().getName().equalsIgnoreCase(event.contact.getName())
-							&& (!ContactLinkedList.retrieve().getEvent().DateAndTime.equals(event.DateAndTime)))
+							&& (!ContactLinkedList.retrieve().getEvent().retrieve().DateAndTime.equals(event.DateAndTime)))
 						return true;
 				}
 				ContactLinkedList.FindNext();
@@ -41,13 +36,33 @@ public class Event extends PhoneBook {
 		}
 		if (ContactLinkedList.retrieve().getEvent() != null) 
 			if (ContactLinkedList.retrieve().getName().equalsIgnoreCase(event.contact.getName())
-					&& (!ContactLinkedList.retrieve().getEvent().DateAndTime.equals(event.DateAndTime)))
+					&& (!ContactLinkedList.retrieve().getEvent().retrieve().DateAndTime.equals(event.DateAndTime)))
 				return true;
-		
+
 
 			return false;
 
 	}
+
+	public boolean Exists (Event event){
+		if (eventList.empty())
+		return false;
+		else {
+			if (eventList.retrieve() != null){
+			eventList.FindFirst();
+			while (!eventList.last()){
+				if (eventList.retrieve() == null)
+				return false;
+				if (eventList.retrieve() != null && eventList.retrieve().getDateAndTime().equals(event.DateAndTime) && eventList.retrieve().getEventTitle().equals(event.eventTitle) && eventList.retrieve().getLocation().equals(event.location) )
+				return true;
+				eventList.FindNext();
+			}}
+			/*if (eventList.retrieve().getDateAndTime().equals(event.DateAndTime) && eventList.retrieve().getEventTitle().equals(event.eventTitle) && eventList.retrieve().getLocation().equals(event.location) && eventList.retrieve() != null)
+				return true;
+			}*/
+				return false;	
+		}
+	};
 
 	public String getEventTitle() {
 		return eventTitle;
@@ -90,8 +105,10 @@ public class Event extends PhoneBook {
 	}
 
 	public String toString() {
-		return "Event title:" + eventTitle + "\nDate and time:" + DateAndTime + "\nlocation:" + location + "\ncontact name:"
+		return "\nevent title:" + eventTitle + "\nDate and time:" + DateAndTime + "\nlocation:" + location + "\ncontact name:"
 				+ contact.getName() ;
 	}
+	
+	
 
 }
